904da2e5b38509f179bf8a0f332413812ea8b42b
lumpov2004 <lumpov2004@mail.ru>
2018-04-15 11:35:15 +0500

    Добавили еще фигуры

--- Tetris/Tetris/Assets/Scripts/FigureController.cs
+++ Tetris/Tetris/Assets/Scripts/FigureController.cs
@@ -17,8 +17,24 @@ public class FigureController : MonoBehaviour
 			case FigureType.Square: 
 				InitSquare();
 				break;
+			case FigureType.Triangle: 
+				InitTriangle();
+				break;
+			case FigureType.LeftZ: 
+				InitLeftZ();
+				break;
+			case FigureType.RightZ: 
+				InitRightZ();
+				break;
+			case FigureType.LeftSix: 
+				InitLeftSix();
+				break;
+			case FigureType.RightSix: 
+				InitRightSix();
+				break;
 		}
 	}
+	#region inits
 	private void InitPlane()
 	{
 		Form = new bool[,]
@@ -37,10 +53,61 @@ public class FigureController : MonoBehaviour
 			{true, true}
 		};
 	}
+	private void InitTriangle()
+	{
+		Form = new bool[,]
+		{
+			{false, true, false},
+			{true, true, true}
+		};
+	}
+	private void InitLeftZ()
+	{
+		Form = new bool[,]
+		{
+			{true, false},
+			{true, true},
+			{false, true}
+		};
+	}
+	private void InitRightZ()
+	{
+		Form = new bool[,]
+		{
+			{false, true},
+			{true, true},
+			{true, false}
+		};
+	}
+	private void InitLeftSix()
+	{
+		Form = new bool[,]
+		{
+			{true, false},
+			{true, true},
+			{true, true}
+		};
+	}
+	private void InitRightSix()
+	{
+		Form = new bool[,]
+		{
+			{false, true},
+			{true, true},
+			{true, true}
+		};
+	}
+	#endregion
 }
 public enum FigureType
 {
 	Plane,
 	Square,
-	
+	Triangle,
+	LeftZ,
+	RightZ,
+	LeftSix,
+	RightSix,
+	LeftG,
+	RightG
 }
