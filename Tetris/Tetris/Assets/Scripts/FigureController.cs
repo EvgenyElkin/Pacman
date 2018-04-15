afbe55f869fd36975f3835e8dcdbd95f50a2980b
lumpov2004 <lumpov2004@mail.ru>
2018-04-15 11:12:56 +0500

    Добавили фигуры

---
+++ Tetris/Tetris/Assets/Scripts/FigureController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FigureController : MonoBehaviour
+{
+	public FigureType Type;
+	public bool[,] Form;
+	
+	public void Start()
+	{
+		switch(Type)
+		{
+			case FigureType.Plane: 
+				InitPlane();
+				break;
+			case FigureType.Square: 
+				InitSquare();
+				break;
+		}
+	}
+	private void InitPlane()
+	{
+		Form = new bool[,]
+		{
+			{true},
+			{true},
+			{true},
+			{true}
+		};
+	}
+	private void InitSquare()
+	{
+		Form = new bool[,]
+		{
+			{true, true},
+			{true, true}
+		};
+	}
+}
+public enum FigureType
+{
+	Plane,
+	Square,
+	
+}
